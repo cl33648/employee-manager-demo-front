a746d52435992834e9526bac7ade1fa74116e23a
Conrad <conrad.lee@utexas.edu>
2021-03-02 21:36:24 -0600

    added html template for UI & button functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,35 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    //if button is add employee
+    if(mode === 'add'){
+      //data-target will be id 'addEmployeeModal' - same in app.component.html
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+
+    //if button is edit employee
+    if(mode === 'edit'){
+      //data-target will be id 'updateEmployeeModal' - same in app.component.html
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+
+    //of button is delete employee
+    if(mode === 'delete'){
+      //data-target will be id 'deleteEmployeeModal' - same in app.component.html
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    //add the button to the 'main-container' in app.component.html
+    //now the button exists in the html
+    container.appendChild(button);
+    button.click();
+  }
 }
